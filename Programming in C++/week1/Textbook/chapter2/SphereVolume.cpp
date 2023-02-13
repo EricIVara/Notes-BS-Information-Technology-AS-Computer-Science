d1ab51b8ee10d941e62fe2015a6cbeb5761fc1d1
eric vara <vara.ada91@gmail.com>
2023-02-13 16:34:27 -0600

    completed Sphere Volume Assignment

---
+++ Programming in C++/week1/Textbook/chapter2/SphereVolume.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+    Given sphereRadius and piVal, compute the volume of a sphere and assign sphereVolume
+    with the result. Use (4.0 / 3.0) to perform floating-point division, instead of
+    (4 / 3) which performs integer division.
+
+    Volume of sphere = (4.0 / 3.0) π r3 (Hint: r3 can be computed using *)
+
+*/
+
+int main() {
+   double piVal = 3.14159;
+   double sphereVolume;
+   double sphereRadius;
+
+   cin >> sphereRadius;
+
+   /* Your solution goes here  */
+   sphereVolume = (4.0 / 3.0) * piVal * (sphereRadius * sphereRadius * sphereRadius);
+
+   cout << sphereVolume << endl;
+
+   return 0;
+}
