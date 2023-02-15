fb46008c0e81877448319a9bdff98d0e995b07d3
eric vara <vara.ada91@gmail.com>
2023-02-14 21:20:11 -0600

    complete if else project

---
+++ Programming in C++/chapter3/SeniorCitizen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int patronAge;
+
+   patronAge = 55;
+
+   /* Your solution goes here  */
+   if(patronAge >= 55) {
+      cout << "Senior citizen\n";
+   } else {
+      cout << "Not senior citizen\n";
+   }
+
+   return 0;
+}
