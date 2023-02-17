fde23241996ca8f3823b12f4fc2c2aa09b6bc7a8
eric vara <vara.ada91@gmail.com>
2023-02-17 17:53:24 -0600

    updated AbbreviationCode.cpp project

--- Programming in C++/projects/AbbreviationCode.cpp
+++ Programming in C++/projects/AbbreviationCode.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <string> // Supports use of "string" data type
+#include <cctype> // library to use the std::tolower function
 using namespace std;
 
 int main() {
-    string userInput;
+  string userInput;
 
-    cout << "Input an abbreviation: " << endl;
-    cin >> userInput;
+  cout << "Input an abbreviation: " << endl;
+  cin >> userInput;
 
-    if((userInput == "LOL") || (userInput == "lol")) {
-        cout << "laughing out loud" << endl;
-    } else if ((userInput == "BFF") || (userInput == "bff")) {
-        cout << "best friends forever" << endl;
-    } else if ((userInput == "IMHO") || (userInput == "imho")) {
-        cout << "in my humble opinion" << endl;
-    } else if ((userInput == "TMI") || (userInput == "tmi")) {
-        cout << "too much information" << endl;
-    } else {
-        cout << "Not a valid input" << endl;
-    }
-    
-    return 0;
+  for (int i = 0; i < userInput.length(); i++) {
+    userInput[i] = tolower(userInput[i]);
+  }
+
+  if (userInput == "lol") {
+    cout << "laughing out loud" << endl;
+  } else if (userInput == "bff") {
+    cout << "best friends forever" << endl;
+  } else if (userInput == "imho") {
+    cout << "in my humble opinion" << endl;
+  } else if (userInput == "tmi") {
+    cout << "too much information" << endl;
+  } else {
+    cout << "Not a valid input" << endl;
+  }
+
+  return 0;
 }
