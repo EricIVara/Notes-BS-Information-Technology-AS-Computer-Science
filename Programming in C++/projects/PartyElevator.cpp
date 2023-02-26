e85b2887153bf355dc6ef0e2f57ed3b77173f4aa
eric vara <vara.ada91@gmail.com>
2023-02-25 20:07:11 -0600

    Updates

--- Programming in C++/projects/PartyElevator.cpp
+++ Programming in C++/projects/PartyElevator.cpp
@@ -23,24 +23,26 @@ int main() {
   const int MAX_WEIGHT = 2500;
   int numGuests = 0, ibsPerGuest = 0, currentWeight = 0;
 
-  cout << "Number of guests: ";
-  cin >> numGuests;
-  cout << endl;
+  while (currentWeight < MAX_WEIGHT) {
+    cout << "Number of guests: ";
+    cin >> numGuests;
+    cout << endl;
 
-  for (int i = 1; i <= numGuests; i++) {
-    cout << "Enter the weight of guest " << i << ": ";
-    cin >> ibsPerGuest;
-    currentWeight += ibsPerGuest;
-    cout << "Current weight: " << currentWeight << endl << endl;
-  }
+    for (int i = 1; i <= numGuests; i++) {
+      cout << "Enter the weight of guest " << i << ": ";
+      cin >> ibsPerGuest;
+      currentWeight += ibsPerGuest;
+      cout << "Current weight: " << currentWeight << endl << endl;
+    }
 
-  if (currentWeight < MAX_WEIGHT) {
-    cout << "Let's go" << endl;
-  } else {
-    cout << "Current weight: " << currentWeight << endl
-         << endl
-         << "This elevator is too heavy by " << currentWeight - MAX_WEIGHT + 1
-         << " pounds." << endl;
+    if (currentWeight < MAX_WEIGHT) {
+      cout << "Let's go" << endl << endl;
+    } else {
+      cout << "Current weight: " << currentWeight << endl
+           << endl
+           << "This elevator is too heavy by " << currentWeight - MAX_WEIGHT + 1
+           << " pounds.\nPlease step Off the elevator." << endl;
+    }
   }
 
   return 0;
