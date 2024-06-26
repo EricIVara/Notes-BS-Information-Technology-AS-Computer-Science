1fd8c3361a2cf92ae8323953e494d84323fb0e4e
eric vara <vara.ada91@gmail.com>
2024-06-25 20:53:20 -0500

    added three classses worth of work

--- university-of-arizon/classes/13-CPT307DataStructures&Algorithms/week1/CalculatePay/CalculatePay/src/PayrollCalculator.java
+++
@@ -1,5 +0,0 @@
-public interface PayrollCalculator {
-    void calculateGrossPay(Employee employee);
-    void calculateDeductions(Employee employee);
-    void calculateNetPay(Employee employee);
-}
